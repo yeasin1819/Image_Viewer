361a16df9c9e505b59566fd974490f1af5259638
Yeasin Arafat <mdarafat1819@gmail.com>
2021-12-11 13:36:13 +0600

    Contanier Added

--- lib/main.dart
+++ lib/main.dart
@@ -29,8 +29,8 @@ class _ImagesState extends State<Images> {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Row(
-        children: [
+      child: Container(
+        child:
             Expanded(
             child: Padding(
               padding: const EdgeInsets.all(16.0),
@@ -47,7 +47,6 @@ class _ImagesState extends State<Images> {
                   child: Image.asset('images/' + (Random().nextInt(20) + 1).toString() + '.png')),
             ),
           ),
-        ],
       ),
     );
   }
